b69aedcd1beb5b70b1c43e78c1599daf7bf43864
João Victor <victor_campelo@outlook.com>
2019-04-04 11:48:13 -0300

    Cadastro de Clientes
    Modificado todo o arquivo para cadastrar clientes no arrayList

--- Main.java
+++ Main.java
@@ -1,11 +1,25 @@
 import java.util.Arrays;
-
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
+		Menu mMenu =  new Menu();
+		int op=0;
+		do {
+			System.out.println("Digite o valor: ");
+			Scanner s1 = new Scanner(System.in);
+			int valor = Integer.parseInt(s1.nextLine());
+			if(valor == 1){
+					Scanner s2 = new Scanner(System.in);
+					String nome = s2.nextLine();
+					System.out.println(nome);
+					mMenu.getClientes().add(nome);
+				}
+			System.out.println( op + "foi selecionada");
+		} while(op!=4);
 		System.out.println("Fim");
 	}
-
 }
--- Menu.java
+++ Menu.java
@@ -7,10 +7,13 @@ import java.util.Scanner;
 public class Menu {
 	private String title;
 	private List<String> options;
+	private ArrayList<String> cliente = new ArrayList();
 
-	public Menu(List<String> options) {
-		this.title = "Menu";
-		this.options = options;
+	public Menu() {
+	}
+
+	public ArrayList getClientes(){
+		return cliente;
 	}
 
 	public Menu(String title, List<String> options) {
@@ -18,30 +21,4 @@ public class Menu {
 		this.options = options;
 	}
 
-	public int getSelection() {
-		int op = 0;
-		while (op==0){
-			System.out.println(title+"\n");
-			int i=1;
-			for (String option : options) {
-				System.out.println(i++ + " - " + option);
-			}
-
-			System.out.println("Informe a opcao desejada. ");
-			Scanner s = new Scanner(System.in);
-			String str = s.nextLine();
-			try {
-				op = Integer.parseInt(str);
-			}
-			catch (NumberFormatException e) {
-				op =0;
-			}
-			if (op>=i){
-				System.out.println("Opcao errada!");
-				op=0;
-			}
-
-		}
-		return op;
-	}
 }
