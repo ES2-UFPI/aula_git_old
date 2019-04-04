26b8d062f1ff5acf2eead3b4dff61713201bb732
root <root@localhost.localdomain>
2019-04-04 14:54:17 -0300

    Classes Cliente e Conta. Main alterado.

---
+++ Cliente.java
@@ -0,0 +1,18 @@
+public class Cliente {
+
+  private double saldo = 0;
+  private int num_agencia;
+
+  private int getAgencia() {
+    return this.num_agencia;
+  }
+
+  private void setSaldo(double valor) {
+    this.saldo = valor;
+  }
+
+  private double getSaldo() {
+    return this.saldo;
+  }
+
+} // fim Cliente
