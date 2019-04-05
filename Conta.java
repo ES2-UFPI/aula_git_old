11f0456290cd603d3cb3fc8cc97e5c4ff027611d
João Pedro <pedrochaveslimas3@gmail.com>
2019-04-04 22:27:58 -0300

    Refactoring Conta.java
    Refactoring Conta.java to include addOperation() method

--- Conta.java
+++ Conta.java
@@ -1,5 +1,6 @@
 public class Conta {
     private Double saldo;
+    private String historico;
 
     public Conta(Double saldo) {
         this.saldo = saldo;
@@ -9,7 +10,16 @@ public class Conta {
         return this.saldo;
     }
 
+    public String getHistorico() {
+        return this.historico;
+    }
+
     public void setSaldo(double saldo) {
         this.saldo = saldo;
+    }
+
+    public void addOperation(Operacao op) {
+        String str = "Tipo: " + op.getTipo() + "\nValor:" + op.getValor() + "\nDest:" + op.getDestinatario() + "\n";
+		this.historico += str;
     }
 }
