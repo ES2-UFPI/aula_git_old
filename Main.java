7af14780b5f46153df34716aec8ac28870f0025d
anapaula <anapaula.ds.mendes@gmail.com>
2019-04-04 22:52:27 -0300

    Solve issues #1 and #2

--- Main.java
+++ Main.java
@@ -3,9 +3,8 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
+		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Cadastrar Cliente", "Abertura de Conta", "Depósito", "Saque", "Transferência"));
 		System.out.println(mainMenu.getSelection() + "foi selecionada");
-		System.out.println("Fim");
 	}
 
 }
